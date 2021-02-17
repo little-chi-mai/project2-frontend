9e9f89d71720d75df7d0b4495157019f7b02a4e3
Anchorite1001 <yangwu.1415@gmail.com>
2021-02-18 00:52:52 +1100

    eventcreate & attendants

--- src/components/EventCreate.js
+++ src/components/EventCreate.js
@@ -50,13 +50,27 @@ class EventCreateForm extends Component {
     };
     console.log(newEvent)
     axios.post(SERVER_URL, newEvent).then((response) => {
-      console.log(response.data)
+      console.log(response);
+      this.addAttendants(attendants, newEvent)
     })
+  }
 
+  addAttendants(attendants, newEvent){
+    console.log(attendants)
+    console.log(this.state.attendants)
     axios.get(SERVER_URL + '.json').then((response) => {
-      const event = response.data.filter(eventInfo => eventInfo.title === newEvent.title && eventInfo.user === newEvent.user && eventInfo.date === newEvent.date)
-      
       console.log(response.data)
+      const found = response.data.find((event) => {
+        return event.title === newEvent.title
+      })
+      console.log(found)
+      attendants.map((user) => {
+        let newAttendant = {user_id: user.id, event_id: found.id};
+        console.log(newAttendant)
+        axios.post('http://localhost:3000/attendants', newAttendant).then((response) => {
+          console.log(response)
+        })
+      })
     })
   }
 
@@ -86,12 +100,12 @@ class EventCreateForm extends Component {
     let user_id = Number(event.target.id)
     if(event.target.checked === true) {
       let add = searched.find((user) => {return user.id === user_id})
-      checked = checked.concat( {user:add})
+      checked = checked.concat( add)
       this.setState({
         attendants:checked
       })
     } else {
-      let remove = checked.findIndex((user) => {return user.user.id === user_id})
+      let remove = checked.findIndex((user) => {return user.id === user_id})
       if(remove !== -1) {
         checked.splice(remove,1)
         this.setState({
@@ -108,16 +122,19 @@ class EventCreateForm extends Component {
         <h3>Venue:</h3>
         <form onSubmit = {this._handleSubmit}>
           <label>Title</label>
-          <input name='title' onChange = {this._handleChange}/>
+          <input name='title' onChange = {this._handleChange} required/>
           <label>Date</label>
-          <input placeholder='2020-12-31' name='date' onChange = {this._handleChange}/>
+          <input placeholder='2020-12-31' name='date' onChange = {this._handleChange} required/>
           <label>Summary</label>
           <textarea name='introduction'onChange = {this._handleChange}/>
           <div>
             <label>Attendants</label>
             <input name='attendants' onChange = {this._findAttendants}/>
             <ul>
-              {this.state.attendants_search.map((user) => (
+              {this.state.attendants.map((attendant) => (
+                <label><input type='checkbox' key = {attendant.id} id = {attendant.id} onChange={this._handleCheckbox} checked />{attendant.name}</label>
+              ))}
+                {this.state.attendants_search.map((user) => (
                 <label><input type='checkbox' key = {user.id} id = {user.id} onChange={this._handleCheckbox}/>{user.name}</label>
               ))}
             </ul>
