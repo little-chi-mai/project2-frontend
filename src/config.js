3ea013d5f0a3cf43a1b653294a034868e10bee50
Mai Nguyen <chimai.dk@gmail.com>
2021-05-23 10:17:31 +1000

    changed heroku url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 //this file allows the development server URL & deployed server URL to be submstitued automatically when it is deployed
 const prod = {
     // SERVER_URL: 'https://agile-tor-91190.herokuapp.com',
-    SERVER_URL : 'http://localhost:3000'
+    SERVER_URL : 'https://ifeellike-mai.herokuapp.com/'
 };
 
 const dev = {
